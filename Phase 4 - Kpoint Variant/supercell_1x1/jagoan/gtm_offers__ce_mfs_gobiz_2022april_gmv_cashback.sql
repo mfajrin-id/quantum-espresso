553f59feba32400af71c6567105e3c39768fd98f
Muhammad Fajrin <mfajrin.id@gmail.com>
2022-07-10 09:18:16 +0800

    Create gtm_offers__ce_mfs_gobiz_2022april_gmv_cashback.sql

---
+++ Phase 4 - Kpoint Variant/supercell_1x1/jagoan/gtm_offers__ce_mfs_gobiz_2022april_gmv_cashback.sql
@@ -0,0 +1,28 @@
+with campaign_pop_raw as (
+  select 
+    customer_id
+    , customer.main_account_number as account_number
+    , customer.customer_cif as cif
+    , gofood_jago_benefit_amount
+--   from `jago-bank-data-production.data_analytics.datamart_base_customer` customer
+  from `jago-bank-data-production`.`jago_gobiz_data_sharing`.`monthly_gobiz_eligible_merchant_benefit`
+    join `jago-bank-data-production`.`dbt_tests_results`.`data_analytics__datamart_base_customer` customer using(customer_id)
+  where
+    customer.customer_status = 'ACTIVE'
+    and customer.main_account_number is not null
+    and gofood_jago_benefit_flag is true)
+
+SELECT 
+  account_number AS accountNumber
+  , 'BC002' AS bankCode
+  , cif
+  , customer_id AS customerId
+  , cast(gofood_jago_benefit_amount as numeric) as amount
+  , TO_BASE64(SHA256(CONCAT(customer_id, gofood_jago_benefit_amount,'GoFood Commission Cashback', FORMAT_DATE("%Y-%m", current_date())))) AS idempotencyKey
+  , STRUCT (
+      STRUCT (
+        'jago://digitalbanking.com/bell-notification/list' AS deepLink
+        , 'https://jagobanking.onelink.me/9ryZ/69f9c122' AS webLink
+      ) AS extra
+    ) AS notificationPayload
+FROM campaign_pop_raw
